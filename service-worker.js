03120b80312628a5dab56af7543f2486a980c11f
fabiomoura7 <fabio-ti@hotmail.com>
2025-05-25 00:37:41 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,33 @@
+const CACHE_NAME = 'controle-estoque-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Caso tenha arquivos CSS/JS externos, adiciona aqui
+];
 
-self.addEventListener('install', function(e) {
-  console.log('Service Worker instalado');
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  // Limpar caches antigos
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
 });
